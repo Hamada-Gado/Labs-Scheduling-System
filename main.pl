ce5cf962a5e44e6a268310952f1d087529f0dd10
Hamada-Gado <ahmedgado0100@gmail.com>
2023-04-24 22:44:24 +0200

    update slot_assignment & day_schedule

--- main.pl
+++ main.pl
@@ -1,5 +1,5 @@
 /**
-ta_slot_assignment(TAs,RemTAs,Name) such that:
+ta_slot_assignment(TAs, RemTAs, Name) such that:
     • TAs and RemTAs are lists of TA structures
     • Name is a name of a TA in TAs.
 ta_slot_assignment/3 succeeds if RemTAs is the list of TA structures resulting
@@ -14,7 +14,7 @@ ta_slot_assignment([ta(Name1, Slots)|T1], [ta(Name1, Slots)|T2], Name2) :-
     ta_slot_assignment(T1, T2, Name2).
 
 /**
-slot_assignment(LabsNum,TAs,RemTAs,Assignment) such that:
+slot_assignment(LabsNum, TAs, RemTAs, Assignment) such that:
     • LabsNum is a number representing the amount of parallel labs in this slot.
     • TAs is a list of TAs structures.
     • RemTAs is the updated list of TAs structures after the assignment to this slot.
@@ -22,20 +22,17 @@ slot_assignment(LabsNum,TAs,RemTAs,Assignment) such that:
 slot_assignment/4 succeeds if Assignment is a possible assignment to a single
 slot with LabsNum labs and RemTAs is the list of modified TAs after the assignment.
 */
-slot_assignment(LabsNum, TAs, RemTAs, Assignment) :-
-    permutation(TAs, Per_TAs),
-    slot_assignment_helper(LabsNum, Per_TAs, RemTAs, Assignment).
-
-slot_assignment_helper(0, TAs, TAs, []).
-slot_assignment_helper(LabsNum, [ta(Name, Slot1)|T1], [ta(Name, Slot2)|T2], [Name|Assignment]) :-
+slot_assignment(0, TAs, TAs, []).
+slot_assignment(LabsNum, TAs, [ta(Name, Slots)|FinalRemTAs], [Name|Assignment]) :-
+    LabsNum > 0,
     New_LabsNum is LabsNum - 1,
-    ta_slot_assignment([ta(Name, Slot1)|T1], [ta(Name, Slot2)|_], Name),
-    slot_assignment_helper(New_LabsNum, T1, T2, Assignment).
-slot_assignment_helper(LabsNum, [ta(Name, 0)|T1], [ta(Name, 0)|T2], Assignment) :-
-    slot_assignment_helper(LabsNum, T1, T2, Assignment).
+    select(ta(Name, _), TAs, RemTAs1),
+    ta_slot_assignment(TAs, RemTAs2, Name),
+    select(ta(Name, Slots), RemTAs2, _),
+    slot_assignment(New_LabsNum, RemTAs1, FinalRemTAs, Assignment).
 
 /**
-max_slots_per_day(DaySched,Max) such that:
+max_slots_per_day(DaySched, Max) such that:
     • DaySched is a day schedule showing the assignment of the TAs in every slot.
     • Max is a number showing the maximum amount of labs a TA can be assignedin a day.
 max_slots_per_day/2 succeeds if no TA is assigned more than Max labs in DaySched.
@@ -63,7 +60,7 @@ max_ta_slots_per_day([Assignment|Rest], Name, Count, Max) :-
     max_ta_slots_per_day(Rest, Name, Count, Max).
 
 /**
-day_schedule(DaySlots,TAs,RemTAs,Assignments) such that:
+day_schedule(DaySlots, TAs, RemTAs, Assignments) such that:
     • DaySlots is a list of 5 numbers representing the number of parallel labs in the
     5 slots of the day.
     • TAs and RemTAs are lists of TA structures.
@@ -72,13 +69,16 @@ day_schedule(DaySlots,TAs,RemTAs,Assignments) such that:
 day_schedule/4 succeeds if Assignment is a possible day assignment given the
 available DaySlots and list of course TAs, while RemTAs is the list of updated TA structures after the day assignment.
 */
+% day_schedule(DaySlots, TAs, RemTAs, Assignment) :-
+%     permutation(TAs, PerTAs),
+%     day_schedule_helper(DaySlots, PerTAs, RemTAs, Assignment).
 day_schedule([], TAs, TAs, []).
-day_schedule([LabsNum|RestDaySlots], TAs, RemTAs, [Assignment|Assignments]) :-
-    slot_assignment(LabsNum, TAs, RemTAs2, Assignment),
-    day_schedule(RestDaySlots, RemTAs2, RemTAs, Assignments).
+day_schedule([LabsNum|RestDaySlots], TAs, FinalRemTAs, [Assignment|Assignments]) :-
+    slot_assignment(LabsNum, TAs, RemTAs, Assignment),
+    day_schedule(RestDaySlots, RemTAs, FinalRemTAs, Assignments).
 
 /**
-week_schedule(WeekSlots,TAs,DayMax,WeekSched) such that:
+week_schedule(WeekSlots, TAs, DayMax, WeekSched) such that:
     • WeekSlots is a list of 6 lists with each list representing a working day from
     Saturday till Thursday. A list representing a day is composed of 5 numbers
     representing the 5 slots in the day. The number at position i in a day list represents the number of parallel labs at slot i.
